ba3c5cc1008ce2ad8de5ebd836ab5d5dc0c7aa8c
bas <ba.abrahamse@student.avans.nl>
2020-05-12 22:27:00 +0200

    initial commit

---
+++ ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/assignment_sm.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###########################################################
+#               WARNING: Generated code!                  #
+#              **************************                 #
+# Manual changes may get lost if file is generated again. #
+# Only code inside the [MANUAL] tags will be kept.        #
+###########################################################
+
+from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
+from ariac_flexbe_states.start_assignment_state import StartAssignment
+from ariac_flexbe_states.end_assignment_state import EndAssignment
+from ariac_flexbe_states.srdf_state_to_moveit_ariac_state import SrdfStateToMoveitAriac
+from flexbe_states.wait_state import WaitState
+from ariac_flexbe_states.detect_part_camera_ariac_state import DetectPartCameraAriacState
+from ariac_flexbe_states.compute_grasp_ariac_state import ComputeGraspAriacState
+from ariac_flexbe_states.moveit_to_joints_dyn_ariac_state import MoveitToJointsDynAriacState
+from ariac_flexbe_states.get_object_pose import GetObjectPoseState
+from ariac_flexbe_behaviors.notify_shipment_ready_sm import notify_shipment_readySM
+# Additional imports can be added inside the following tags
+# [MANUAL_IMPORT]
+
+# [/MANUAL_IMPORT]
+
+
+'''
+Created on Tue May 12 2020
+@author: Bas Abrahamse
+'''
+class AssignmentSM(Behavior):
+	'''
+	Test
+	'''
+
+
+	def __init__(self):
+		super(AssignmentSM, self).__init__()
+		self.name = 'Assignment'
+
+		# parameters of this behavior
+
+		# references to used behaviors
+		self.add_behavior(notify_shipment_readySM, 'notify_shipment_ready')
+
+		# Additional initialization code can be added inside the following tags
+		# [MANUAL_INIT]
+		
+		# [/MANUAL_INIT]
+
+		# Behavior comments:
+
+
+
+	def create(self):
+		# x:37 y:662, x:476 y:317
+		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
+		_state_machine.userdata.conveyor_belt_power = 100.0
+		_state_machine.userdata.config_name_home = 'home'
+		_state_machine.userdata.move_group = 'manipulator'
+		_state_machine.userdata.move_group_prefix = '/ariac/arm1'
+		_state_machine.userdata.action_topic = '/move_group'
+		_state_machine.userdata.robot_name = ''
+		_state_machine.userdata.camera_ref_frame = 'arm1_linear_arm_actuator'
+		_state_machine.userdata.camera_topic = '/ariac/logical_camera_6'
+		_state_machine.userdata.camera_frame = 'logical_camera_6_frame'
+		_state_machine.userdata.part = 'gasket_part'
+		_state_machine.userdata.config_name_bin6PreGrasp = 'bin6PreGrasp'
+		_state_machine.userdata.tool_link = 'ee_link'
+		_state_machine.userdata.part_offset = 0.1
+		_state_machine.userdata.part_rotation = 0
+		_state_machine.userdata.config_name_tray1PreDrop = 'tray1PreDrop'
+		_state_machine.userdata.agv_pose = []
+
+		# Additional creation code can be added inside the following tags
+		# [MANUAL_CREATE]
+		
+		# [/MANUAL_CREATE]
+
+
+		with _state_machine:
+			# x:71 y:109
+			OperatableStateMachine.add('StartAssignment',
+										StartAssignment(),
+										transitions={'continue': 'MoveR1Home'},
+										autonomy={'continue': Autonomy.Off})
+
+			# x:182 y:656
+			OperatableStateMachine.add('EndAssignment',
+										EndAssignment(),
+										transitions={'continue': 'finished'},
+										autonomy={'continue': Autonomy.Off})
+
+			# x:233 y:110
+			OperatableStateMachine.add('MoveR1Home',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'DetectCameraPart', 'planning_failed': 'WaitRetry1', 'control_failed': 'WaitRetry1', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_home', 'move_group': 'move_group', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:233 y:8
+			OperatableStateMachine.add('WaitRetry1',
+										WaitState(wait_time=5),
+										transitions={'done': 'MoveR1Home'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:438 y:112
+			OperatableStateMachine.add('DetectCameraPart',
+										DetectPartCameraAriacState(time_out=5),
+										transitions={'continue': 'MoveR1PreGrasp1', 'failed': 'failed', 'not_found': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off, 'not_found': Autonomy.Off},
+										remapping={'ref_frame': 'camera_ref_frame', 'camera_topic': 'camera_topic', 'camera_frame': 'camera_frame', 'part': 'part', 'pose': 'pose'})
+
+			# x:641 y:111
+			OperatableStateMachine.add('MoveR1PreGrasp1',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'ComputePick', 'planning_failed': 'WaitRetry2', 'control_failed': 'WaitRetry2', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_bin6PreGrasp', 'move_group': 'move_group', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:640 y:6
+			OperatableStateMachine.add('WaitRetry2',
+										WaitState(wait_time=5),
+										transitions={'done': 'MoveR1PreGrasp1'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:840 y:113
+			OperatableStateMachine.add('ComputePick',
+										ComputeGraspAriacState(joint_names=['linear_arm_actuator_joint', 'shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']),
+										transitions={'continue': 'MoveR1ToPick1', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'move_group': 'move_group', 'move_group_prefix': 'move_group_prefix', 'tool_link': 'tool_link', 'pose': 'pose', 'offset': 'part_offset', 'rotation': 'part_rotation', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:1023 y:110
+			OperatableStateMachine.add('MoveR1ToPick1',
+										MoveitToJointsDynAriacState(),
+										transitions={'reached': 'WachtEven', 'planning_failed': 'WaitRetry3', 'control_failed': 'WaitRetry3'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
+										remapping={'move_group_prefix': 'move_group_prefix', 'move_group': 'move_group', 'action_topic': 'action_topic', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:1215 y:107
+			OperatableStateMachine.add('WaitRetry3',
+										WaitState(wait_time=5),
+										transitions={'done': 'MoveR1ToPick1'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:1022 y:200
+			OperatableStateMachine.add('WachtEven',
+										WaitState(wait_time=1),
+										transitions={'done': 'MoveR1PreGrasp2'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:1017 y:280
+			OperatableStateMachine.add('MoveR1PreGrasp2',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'MoveR1PreDrop', 'planning_failed': 'WaitRetry4', 'control_failed': 'WaitRetry4', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_bin6PreGrasp', 'move_group': 'move_group', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:1215 y:278
+			OperatableStateMachine.add('WaitRetry4',
+										WaitState(wait_time=5),
+										transitions={'done': 'MoveR1PreGrasp2'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:1005 y:369
+			OperatableStateMachine.add('MoveR1PreDrop',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'GetAgvPose', 'planning_failed': 'WaitRetry5', 'control_failed': 'WaitRetry5', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_tray1PreDrop', 'move_group': 'move_group', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:1210 y:372
+			OperatableStateMachine.add('WaitRetry5',
+										WaitState(wait_time=5),
+										transitions={'done': 'MoveR1PreDrop'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:1002 y:454
+			OperatableStateMachine.add('GetAgvPose',
+										GetObjectPoseState(object_frame='kit_tray_1', ref_frame='arm1_linear_arm_actuator'),
+										transitions={'continue': 'ComputeDrop', 'failed': 'ComputeDrop'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'pose': 'agv_pose'})
+
+			# x:998 y:592
+			OperatableStateMachine.add('ComputeDrop',
+										ComputeGraspAriacState(joint_names=['linear_arm_actuator_joint', 'shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']),
+										transitions={'continue': 'MoveR1ToDrop', 'failed': 'MoveR1ToDrop'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'move_group': 'move_group', 'move_group_prefix': 'move_group_prefix', 'tool_link': 'tool_link', 'pose': 'agv_pose', 'offset': 'part_offset', 'rotation': 'part_rotation', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:661 y:522
+			OperatableStateMachine.add('MoveR1ToDrop',
+										MoveitToJointsDynAriacState(),
+										transitions={'reached': 'notify_shipment_ready', 'planning_failed': 'WaitRetry6', 'control_failed': 'WaitRetry6'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
+										remapping={'move_group_prefix': 'move_group_prefix', 'move_group': 'move_group', 'action_topic': 'action_topic', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:661 y:640
+			OperatableStateMachine.add('WaitRetry6',
+										WaitState(wait_time=5),
+										transitions={'done': 'MoveR1ToDrop'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:418 y:581
+			OperatableStateMachine.add('notify_shipment_ready',
+										self.use_behavior(notify_shipment_readySM, 'notify_shipment_ready'),
+										transitions={'finished': 'EndAssignment', 'failed': 'EndAssignment'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit})
+
+
+		return _state_machine
+
+
+	# Private functions can be added inside the following tags
+	# [MANUAL_FUNC]
+	
+	# [/MANUAL_FUNC]
